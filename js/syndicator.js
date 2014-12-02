6ead4c9f322f9db65fe3ccfc08338af541fda7ce
rorytaca <rorytaca@gmail.com>
2014-12-02 17:31:44 -0500

    Both firebase db's implemented and finalized. Crude responsive UI made

--- js/syndicator.js
+++ js/syndicator.js
@@ -1,28 +1,36 @@
 angular.module('syndicatorApp', ['ngRoute', 'firebase'])
     //firebase first
-    .value('fbURL', 'https://amber-inferno-7558.firebaseio.com/')
-    .factory('Products', function($firebase, fbURL) {
-        return $firebase(new Firebase(fbURL)).$asArray();
+    .value('fbURL1', 'https://amber-inferno-7558.firebaseio.com/')
+    .factory('Products', function($firebase, fbURL1) {
+        return $firebase(new Firebase(fbURL1)).$asArray();
+    })
+    .value('fbURL2', 'https://amber-inferno-7600.firebaseio.com/')
+    .factory('Websites', function($firebase, fbURL2) {
+        return $firebase(new Firebase(fbURL2)).$asArray();
     })
     .config(function($routeProvider) {
         $routeProvider
             .when('/', {
-              controller:'ListCtrl',
-              templateUrl:'views/list.html'
+              controller:'ListProductsCtrl',
+              templateUrl:'views/listProducts.html'
             })
             .when('/newProduct', {
               controller:'CreateCtrl',
               templateUrl:'views/create.html'
             })
             .when('/newSite', {
-              controller:'addSiteCtrl',
+              controller:'AddSiteCtrl',
               templateUrl:'views/addSite.html'
+            })
+            .when('/listSites', {
+              controller:'ListSitesCtrl',
+              templateUrl:'views/listSites.html'
             })
             .otherwise({
               redirectTo:'/'
             });
     })
-    .controller('ListCtrl', function($scope, Products) {
+    .controller('ListProductsCtrl', function($scope, Products) {
         $scope.products = Products;
     })
     .controller('CreateCtrl', function($scope, $location, Products) {
@@ -33,6 +41,14 @@ angular.module('syndicatorApp', ['ngRoute', 'firebase'])
                $location.path('/');
             });
       };
-    })    
-    
-    .controller('')
+    })
+        .controller('ListSitesCtrl', function($scope, Websites) {
+        $scope.websites = Websites;
+    })
+    .controller('AddSiteCtrl', function($scope, $location, Websites) {
+        $scope.save = function() {
+            Websites.$add($scope.website).then(function(data) {
+               $location.path('/listSites');
+            });
+      };
+    }) 
