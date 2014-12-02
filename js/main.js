413efd8b39a2d78a1a99aaa86d710efba3d3b7ca
rorytaca <rorytaca@gmail.com>
2014-12-02 14:27:11 -0500

    Project cropped: Initial project file built off of different project, removed all unnecessary resources

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 $(window).resize(function() {
-    //Watch for resizes while collapsed navbar is out
-    console.log('resize');
-    if (window.innerWidth < 760) {
-        //if has class "in"
-        if ($("#navbar-collapse-1").hasClass("in")) {
-            $("#navbar-collapse-1").toggleClass("in");
-        }
-    }
 
 });
